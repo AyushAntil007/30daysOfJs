a361496c2d4bbf8c54cdb04f4974680bfec1ae45
AyushAntil007 <ayushantil2023@gmail.com>
2024-07-19 15:25:56 +0530

    added day1.js file

---
+++ day1.js
@@ -0,0 +1,58 @@
+// Variables And Datatypes
+
+//Activity-1 : Variable Declaration
+
+//Task-1
+var x=123;
+console.log(x);      //Output-123
+
+
+//Task-2
+let str='Ayush';
+console.log(str);    //Output-Ayush
+
+
+
+//Activity-2 : Constant Declaration
+
+//Task-3
+const check=true;
+console.log(check);  //Output:true
+
+
+//Activity-3 : Data Types
+
+//Task-4
+let num=21;
+console.log(typeof(num)); //Output:number
+
+let string='OCD';
+console.log(typeof(string)); //Output:string
+
+let bool=true;
+console.log(typeof(bool)); //Output:boolean
+
+let arr=[1,2,3,4];
+console.log(typeof(arr)); //Output:object
+
+
+//Activity-4 : Reassigning Variables
+
+//Task-5
+let k=66;
+console.log(k);       //output:66
+k=77;
+console.log(k);       //output:77
+
+
+//Activity-5 : Understanding const
+
+//Task-6
+const z=76;
+console.log(z);       //output:76
+z=77;
+console.log(z);       //output:TypeError: Assignment to constant variable.
+
+
+
+
